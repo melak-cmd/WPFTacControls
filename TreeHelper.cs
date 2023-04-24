b43ff799ad43bfca746c0def46e6b31311f2ee46
Mohamed El Amine <a18m02k86_pro@outlook.com>
2023-04-24 10:59:54 +0200

    Initial commit

---
+++ TreeHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.Surface.Presentation.Controls;
+
+namespace AFTERMOUSE
+{
+    public class TreeHelper
+    {
+        public static DependencyObject getItemParent(DependencyObject source, Type type)
+        {
+            DependencyObject parent = source;
+            while (parent != null && parent.GetType() != type)
+            {
+                parent = VisualTreeHelper.GetParent((DependencyObject)parent);
+                if (parent == null)
+                {
+                    return null;
+                }
+            }
+            if (parent.GetType() == type)
+            {
+                return parent;
+            }
+            return null;
+        }
+    }
+}
